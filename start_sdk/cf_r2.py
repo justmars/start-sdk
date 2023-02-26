bad1520ab0646ba9f5e14c6364e73cf11a76b527
justmars <39538958+justmars@users.noreply.github.com>
2023-02-26 20:10:24 +0800

    feat: added filter, prefixes to r2

--- start_sdk/cf_r2.py
+++ start_sdk/cf_r2.py
@@ -1,6 +1,8 @@
+from collections.abc import Iterator
 from pathlib import Path
 from pydantic import BaseSettings, Field
 import boto3
+from boto3.resources.collection import ResourceCollection
 
 
 class CFR2(BaseSettings):
@@ -82,6 +84,29 @@ class CFR2_Bucket(CFR2):
     @property
     def bucket(self):
         return self.get_bucket(self.name)
+
+    @property
+    def client(self):
+        return self.bucket.meta.client
+
+    def top_prefixes(self):
+        """See adapted recipe from boto3 re: top-level [prefixes](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#list-top-level-common-prefixes-in-amazon-s3-bucket)."""  # noqa: E501
+        _objs = []
+        paginator = self.client.get_paginator("list_objects")
+        result = paginator.paginate(Bucket=self.name, Delimiter="/")
+        for prefix in result.search("CommonPrefixes"):
+            _objs.append(prefix.get("Prefix"))  # type: ignore
+        return _objs
+
+    def filter(self, prefix: str, key: str | None = None) -> Iterator[str]:
+        """See modified recipe from boto3 re: [filtering](https://boto3.amazonaws.com/v1/documentation/api/latest/guide/collections.html#filtering). If a `key` is supplied with the `prefix`, a secondary filter is applied."""  # noqa: E501
+        objs = self.bucket.objects.filter(Prefix=prefix)
+        for obj in objs:
+            if key:
+                if key in obj.key:
+                    yield obj.key
+            else:
+                yield obj.key
 
     def upload(self, file_like: str | Path, loc: str, args: dict = {}):
         """Upload local `file_like` contents to r2-bucket path `loc`.
